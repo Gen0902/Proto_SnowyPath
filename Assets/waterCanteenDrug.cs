146864d9ff6595994920249059d38bee73f730bb
TheophileCatella <theophile.catella@gmail.com>
2020-12-13 22:31:57 +0100

    spikeField + Canteen script + chest opening script

---
+++ Assets/waterCanteenDrug.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class waterCanteenDrug : MonoBehaviour
+{
+    bool hasBeenDrunk = false;
+
+    void OnTriggerEnter(Collider playerCollider)
+    {
+        
+        if (hasBeenDrunk == false)
+        {
+            GetComponent<AudioSource>().Play();
+            print("Everything seems different.");
+
+            GetComponent<MeshRenderer>().enabled = false;
+            hasBeenDrunk = true;
+        }
+    }
+}
